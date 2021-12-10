ee2419c91b72acf05106e610ebfef896b4537769
Luke Bayliss <lbayliss@scottlogic.com>
2021-12-10 11:24:29 +0000

    Created separate class to hold test data with get methods

--- src/test/java/com/scottlogic/SortingTests/AuthorSortingTests.java
+++ src/test/java/com/scottlogic/SortingTests/AuthorSortingTests.java
@@ -2,6 +2,7 @@ package com.scottlogic.SortingTests;
 
 import com.scottlogic.SortContext;
 import com.scottlogic.SortOrder;
+import com.scottlogic.SortingTests.utils.UserPostSortData;
 import com.scottlogic.UserPost;
 import com.scottlogic.sort.SortAuthor;
 import org.junit.jupiter.api.Assertions;
@@ -9,40 +10,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class AuthorSortingTests {
-    UserPost userPost1 = new UserPost("Mick Jagger",
-            OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC),
-            "You got moves like me?", 2);
-
-    UserPost userPost2 = new UserPost("Matt Damon",
-            OffsetDateTime.of(2020, 1, 3, 8, 53, 34, 0, ZoneOffset.UTC),
-            "How you like them apples?!", 1);
-
-    UserPost userPost3 = new UserPost("Jane Smith",
-            OffsetDateTime.of(2020, 3, 12, 13, 22, 12, 0, ZoneOffset.UTC),
-            "An example of a post \nwith lines breaks.", 3);
-    List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3);
 
     SortContext sortAuthor = new SortContext(new SortAuthor());
 
     @Test
     @DisplayName("Ascending functionality")
     void sortAuthorAscending() {
-        List<UserPost> postsToSort = userPosts;
-        List<UserPost> expectedAuthorSort = Arrays.asList(userPost3, userPost2, userPost1);
+        List<UserPost> postsToSort = UserPostSortData.getUnsortedList();
+        List<UserPost> expectedAuthorSort = UserPostSortData.getSortedAuthorAscendingList();
         Assertions.assertEquals(expectedAuthorSort, sortAuthor.sortPosts(postsToSort, SortOrder.ASC));
     }
 
     @Test
     @DisplayName("Descending functionality")
     void sortAuthorDescending() {
-        List<UserPost> postsToSort = userPosts;
-        List<UserPost> expectedAuthorSort = Arrays.asList(userPost1, userPost2, userPost3);
+        List<UserPost> postsToSort = UserPostSortData.getUnsortedList();
+        List<UserPost> expectedAuthorSort = UserPostSortData.getSortedAuthorDescendingList();
         Assertions.assertEquals(expectedAuthorSort, sortAuthor.sortPosts(postsToSort, SortOrder.DESC));
     }
 
--- src/test/java/com/scottlogic/SortingTests/DateSortingTests.java
+++ src/test/java/com/scottlogic/SortingTests/DateSortingTests.java
@@ -2,6 +2,7 @@ package com.scottlogic.SortingTests;
 
 import com.scottlogic.SortContext;
 import com.scottlogic.SortOrder;
+import com.scottlogic.SortingTests.utils.UserPostSortData;
 import com.scottlogic.UserPost;
 import com.scottlogic.sort.SortDate;
 import org.junit.jupiter.api.Assertions;
@@ -9,40 +10,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.List;
 
 public class DateSortingTests {
-    UserPost userPost1 = new UserPost("Mick Jagger",
-            OffsetDateTime.of(2020, 1, 3, 7, 12, 3, 0, ZoneOffset.UTC),
-            "You got moves like me?", 2);
-
-    UserPost userPost2 = new UserPost("Matt Damon",
-            OffsetDateTime.of(2020, 1, 3, 8, 53, 34, 0, ZoneOffset.UTC),
-            "How you like them apples?!", 1);
-
-    UserPost userPost3 = new UserPost("Jane Smith",
-            OffsetDateTime.of(2020, 3, 12, 13, 22, 12, 0, ZoneOffset.UTC),
-            "An example of a post \nwith lines breaks.", 3);
-    List<UserPost> userPosts = Arrays.asList(userPost1, userPost2, userPost3);
 
     SortContext sortDate = new SortContext(new SortDate());
 
     @Test
     @DisplayName("Ascending functionality")
     void sortDateAscending() {
-        List<UserPost> postsToSort = userPosts;
-        List<UserPost> expectedDateSort = Arrays.asList(userPost1, userPost2, userPost3);
+        List<UserPost> postsToSort = UserPostSortData.getUnsortedList();
+        List<UserPost> expectedDateSort = UserPostSortData.getSortedDateAscendingList();
         Assertions.assertEquals(expectedDateSort, sortDate.sortPosts(postsToSort, SortOrder.ASC));
     }
 
     @Test
     @DisplayName("Descending functionality")
     void sortDateDescending() {
-        List<UserPost> postsToSort = userPosts;
-        List<UserPost> expectedDateSort = Arrays.asList(userPost3, userPost2, userPost1);
+        List<UserPost> postsToSort = UserPostSortData.getUnsortedList();
+        List<UserPost> expectedDateSort = UserPostSortData.getSortedDateDescendingList();
         Assertions.assertEquals(expectedDateSort, sortDate.sortPosts(postsToSort, SortOrder.DESC));
     }
 
